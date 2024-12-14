ba9e5ab49bc5a92418896bb95eff9ca8accff5bd
hank <hancockgu1@gmail.com>
2024-12-14 22:25:13 +0800

    add models

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -1,10 +1,32 @@
 # puppyCompanyBlog/__init__.py
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
 
+##################
+#####Datebase Setup
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+##################
+#####LOGIN CONFIGS
+
+login_manager = LoginManager(app)
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+
 from puppycompanyblog.core.views import core
 from puppycompanyblog.error_pages.handlers import error_pages
+
 app.register_blueprint(core)
 app.register_blueprint(error_pages)
